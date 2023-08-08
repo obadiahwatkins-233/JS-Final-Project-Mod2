072ec1f5ffeb9f16ceded54503639b20f47aa550
obadiahwatkins-233 <129129175+obadiahwatkins-233@users.noreply.github.com>
2023-08-07 17:34:39 -0700

    Add files via upload
    Updated Guess the number and Guess the Letter game

--- script.js
+++ script.js
@@ -1,41 +1,112 @@
-// Generate a random number between min and max
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  // Game variables
-  var secretNumber = getRandomNumber(1, 100);
-  var attempts = 0;
-  
-  // Function to check the user's guess
-  function checkGuess() {
-    var userGuess = parseInt(document.getElementById("userGuess").value);
-  
-    if (isNaN(userGuess)) {
-      setMessage("Nice Try!!__That's not a number!!__Please enter a valid number!!");
-      return;
-    }
-  
-    attempts++;
-  
-    if (userGuess === secretNumber) {
-      setMessage(
-        `Congratulations! You guessed the number in ${attempts} attempts.`
-      );
-    } else if (userGuess < secretNumber) {
-      setMessage("Too low! Try a higher number.");
-    } else {
-      setMessage("Too high! Try a lower number.");
-    }
+reset game button
+
+var secretNumber = Math.floor(Math.random() * 100) + 1;
+var attempts = 0;
+
+function checkGuess() {
+  var userGuess = parseInt(document.getElementById("userGuess").value);
+
+  if (isNaN(userGuess)) {
+    document.getElementById("message").textContent = "Please enter a valid number!";
+    return;
   }
 
- // Function to display # of attempts
+  attempts++;
 
-  function displayAttempts() {
-    document.getElementById("message").textContent = "Attempts: " + attempts;
+  if (userGuess === secretNumber) {
+    document.getElementById("message").textContent = "Congratulations! You guessed the number in " + attempts + " attempts.";
+  } else if (userGuess < secretNumber) {
+    document.getElementById("message").textContent = "Too low! Try a higher number.";
+  } else {
+    document.getElementById("message").textContent = "Too high! Try a lower number.";
   }
-  
-  // Function to display messages
-  function setMessage(msg) {
-    document.getElementById("message").textContent = msg;
+}
+
+function resetGame() {
+  secretNumber = Math.floor(Math.random() * 100) + 1;
+  attempts = 0;
+  document.getElementById("message").textContent = "";
+  document.getElementById("userGuess").value = "";
+}
+
+
+
+guess the letter
+
+var secretLetter = getRandomLetter();
+var attempts = 0;
+
+function getRandomLetter() {
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+
+function checkGuess() {
+  var userGuess = document.getElementById("userGuess").value.toUpperCase();
+
+  if (!/^[A-Z]$/.test(userGuess)) {
+    document.getElementById("message").textContent = "Please enter a single letter between A and Z.";
+    return;
   }
+
+  attempts++;
+
+  if (userGuess === secretLetter) {
+    document.getElementById("message").textContent = "Congratulations! You guessed the letter '" + secretLetter + "' in " + attempts + " attempts.";
+  } else if (userGuess < secretLetter) {
+    document.getElementById("message").textContent = "Too low! Try a higher letter.";
+  } else {
+    document.getElementById("message").textContent = "Too high! Try a lower letter.";
+  }
+}
+
+function resetGame() {
+  secretLetter = getRandomLetter();
+  attempts = 0;
+  document.getElementById("message").textContent = "";
+  document.getElementById("userGuess").value = "";
+}
+
+
+display attempts on screen
+
+var secretLetter = getRandomLetter();
+var attempts = 0;
+
+function getRandomLetter() {
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+
+function checkGuess() {
+  var userGuess = document.getElementById("userGuess").value.toUpperCase();
+
+  if (!/^[A-Z]$/.test(userGuess)) {
+    document.getElementById("message").textContent = "Please enter a single letter between A and Z.";
+    return;
+  }
+
+  attempts++;
+
+  if (userGuess === secretLetter) {
+    document.getElementById("message").textContent = "Congratulations! You guessed the letter '" + secretLetter + "' in " + attempts + " attempts.";
+  } else if (userGuess < secretLetter) {
+    document.getElementById("message").textContent = "Too low! Try a higher letter.";
+  } else {
+    document.getElementById("message").textContent = "Too high! Try a lower letter.";
+  }
+
+  displayAttempts();
+}
+
+function resetGame() {
+  secretLetter = getRandomLetter();
+  attempts = 0;
+  document.getElementById("message").textContent = "";
+  document.getElementById("userGuess").value = "";
+  displayAttempts();
+}
+
+function displayAttempts() {
+  document.getElementById("attempts").textContent = "Attempts: " + attempts;
+}
